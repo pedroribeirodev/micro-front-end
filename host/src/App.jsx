d4a9f65547bf1aba495593462cf3289de0a71854
pedroribeirodev <pedro.bonfim.13@gmail.com>
2022-01-03 18:50:46 -0300

    first commit

---
+++ host/src/App.jsx
@@ -0,0 +1,21 @@
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom";
+
+import { Page } from "./Main";
+import ErrorBoundary from "./Error";
+
+const RemoteButton = React.lazy(() => import("dsl/Button"));
+
+const App = () => {
+  return (
+    <Page>
+      <Suspense fallback="Loading..">
+        <ErrorBoundary>
+          <RemoteButton />
+        </ErrorBoundary>
+      </Suspense>
+    </Page>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("app"));
